2639440a7a5a37add451a9389bfa0e5d46904dff
Nina <boldurfaith@gmail.com>
2023-03-31 04:13:06 +0100

    initial commit

--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,14 +1,29 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
+        [Theory]
+        [InlineData("kayak", true)]
+        [InlineData("Breeze", true)]
+        [InlineData("Energy", true)]
+        [InlineData("Palindrome", false)]
+        [InlineData("technodrome", false)]
 
+        public void IsAPalindromeTest(string word, bool expected)
+        {
+            //Arrange (skip step if method is static, so no need to create an instance)
+            var tester = new WordSmith();
+
+            //Act (call the method)
+            var actual = tester.IsAPalindrome(word);
+
+
+            //Assert (verify)
+            Assert.Equal(expected, actual);
         }
     }
 }
